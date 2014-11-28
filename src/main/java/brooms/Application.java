43768dedf100438fe61ed3b283c0ccb98ab68e36
Sbreighs <stefano.bragaglia@gmail.com>
2014-11-28 15:23:51 +0000

    Project gradle structure

--- src/main/java/brooms/Application.java
+++ src/main/java/brooms/Application.java
@@ -24,6 +24,8 @@ public class Application {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		for (String arg : args)
+			System.out.println("> " + arg);
 		System.out.println("Done.");
 	}
 
